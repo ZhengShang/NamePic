07495957d096d42edc3c8c65d6b2ecfc83b7d47c
ZhengShang <shang10023@126.com>
2020-08-22 20:43:16 +0800

    feedback update

--- app/src/main/java/cn/zhengshang/namepic/fragments/FeedbackFragment.kt
+++ app/src/main/java/cn/zhengshang/namepic/fragments/FeedbackFragment.kt
@@ -4,14 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
+import androidx.databinding.DataBindingUtil
 import cn.zhengshang.namepic.BaseFragment
 import cn.zhengshang.namepic.R
+import cn.zhengshang.namepic.databinding.FragFeedbackBinding
 import cn.zhengshang.namepic.tools.MailManager
 import cn.zhengshang.namepic.tools.navClickBack
-import com.dd.processbutton.iml.ActionProcessButton
+import kotlinx.android.synthetic.main.frag_feedback.*
 
 /**
  * Created by shangzheng on 2020/7/28.
@@ -22,16 +23,14 @@ class FeedbackFragment : BaseFragment() {
                               container: ViewGroup?,
                               savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.frag_feedback, container, false)
+        return DataBindingUtil.inflate<FragFeedbackBinding>(inflater,
+                R.layout.frag_feedback, container, false).root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         view.findViewById<Toolbar>(R.id.toolbar).navClickBack()
 
-        val detailText = view.findViewById<EditText>(R.id.detail_text)
-        val contactText = view.findViewById<EditText>(R.id.contact_text)
-        val submit = view.findViewById<ActionProcessButton>(R.id.submit)
         submit.completeText = getString(R.string.faeedback_success)
         submit.errorText = getString(R.string.feedback_failed)
         submit.setOnClickListener {
@@ -42,18 +41,18 @@ class FeedbackFragment : BaseFragment() {
             } else if (progress in 1..99) {
                 return@setOnClickListener
             }
-            if (detailText.text.isEmpty()) {
+            if (detail_text.text.isEmpty()) {
                 Toast.makeText(context, R.string.empty_body, Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
             submit.progress = 50
-            MailManager.instance.sendMail("NamePic Feedback", detailText.text.toString() + "\rContact : " + contactText.text)
+            MailManager.instance.sendMail("NamePic Feedback", detail_text.text.toString() + "\rContact : " + contact_text.text)
             MailManager.instance.setSendStateListener(object : MailManager.SendStateListener {
                 override fun onSendState(state: Boolean) {
                     if (state) {
                         submit.progress = 100
-                        detailText.setText("")
-                        contactText.setText("")
+                        detail_text.setText("")
+                        contact_text.setText("")
                     } else {
                         submit.progress = -1
                     }
